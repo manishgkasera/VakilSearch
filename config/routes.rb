59e772601b78714b44ce563ee95be4207708441d
manishk <manishk@infibeam.net>
2014-04-06 16:22:47 +0530

    Basic service model/controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ VakilSearch::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root 'services#index'
+
+  resources :services, only: [:index] do
+    collection do
+      get 'search'
+    end
+  end
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
