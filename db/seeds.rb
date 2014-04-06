926933edab8315567d2f04a1eb9de1d1bdb092d6
manishk <manishk@infibeam.net>
2014-04-06 17:35:58 +0530

    Adding db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+locations = {}
+lawyers = {}
+CSV.read('db/seeds/Lawyers City List.csv', headers: true).each do |row|
+  location = if locations[row[' Location']]
+    locations[row[' Location']]
+  else
+    locations[row[' Location']] = Location.create!(city: row[' Location'])
+  end
+
+  lawyer = Lawyer.create! code: row['Lawyer Code'],
+                          name: row[' Lawyer Name'],
+                          experiance:  row[' Years of Experience'],
+                          average_rating: row[' Average Rating'],
+                          location: location
+
+  lawyers[lawyer.code] = lawyer
+end
+
+services = {}
+CSV.read('db/seeds/Lawyer Service List.csv', headers: true).each do |row|
+  lawyer = lawyers[row['Lawyer Code']]
+  service = services[row[' Service Code']] || lawyer.services.create!(code: row[' Service Code'],
+                                                                      name: row[' Service Name'],
+                                                                      charge: row[' Charges'])
+  lawyer.services << service
+  services[service.code] ||= service
+end
