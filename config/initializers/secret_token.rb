8fb9cf796f79e68c24db34bbaa7da955bb203e2a
manishk <manishk@infibeam.net>
2014-04-06 16:06:29 +0530

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VakilSearch::Application.config.secret_key_base = '56ca49e0e331f327b348fc280db269e96f776cbbc6ce714492f8014215ed9cc88cd240bbfbc18676b6e696003c9511fcdcdf9e3580764976617d86dc684d8543'
